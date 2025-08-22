c8175c06e4363f8605908b997a32c2c45cd82919
Hong Minh <nguyengochongminh@gmail.com>
2025-08-22 20:46:54 +0700

    hoan thanh bai 6 trong lab01

---
+++ BT_Tuan1/dist/Book.js
@@ -0,0 +1,10 @@
+"use strict";
+class Book {
+    constructor(title, author, year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+}
+const book1 = new Book('Tutorial TypeScript', 'Matta', 2008);
+console.log('Book:', book1.title, book1.author, book1.year);
---
+++ BT_Tuan1/src/Book.ts
@@ -0,0 +1,14 @@
+class Book {
+    public title: string;
+    public author: string;
+    public year: number;
+
+    constructor(title: string, author: string, year: number) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+}
+
+const book1 = new Book('Tutorial TypeScript', 'Matta', 2008);
+console.log('Book:', book1.title, book1.author, book1.year);
