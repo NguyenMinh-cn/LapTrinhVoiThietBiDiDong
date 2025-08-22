1d3364dfb95bc47b9ad773205f74045604359214
Hong Minh <nguyengochongminh@gmail.com>
2025-08-22 21:58:13 +0700

    hoan thanh bai 2 trong lab02 phan A.Basics with Promise

--- lab02/dist/PromiseBasic.js
+++ lab02/dist/PromiseBasic.js
@@ -1,7 +1,17 @@
 "use strict";
+//1.Create a Promise that returns the string "Hello Async" after 2 seconds
 const helloAsync = new Promise((resolve) => {
     setTimeout(() => {
         resolve('Hello Async');
     }, 2000);
 });
-helloAsync.then((m) => console.log(m));
+// helloAsync.then((m) => console.log(m));
+//2. Write a function that returns a Promise resolving with the number 10 after 1 second
+function getNumber() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(10);
+        }, 1000);
+    });
+}
+getNumber().then(console.log);
