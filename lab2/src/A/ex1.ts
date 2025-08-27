2eded3ebb3de876765c14fc1874c820470bb69b1
NguyenNgocHongMinh <nguyengochongminh@gmail.com>
2025-08-27 07:23:00 +0700

    hoan thanh bai 1 trong lab02

---
+++ lab2/src/A/ex1.ts
@@ -0,0 +1,9 @@
+//1.Create a Promise that returns the string "Hello Async" after 2 seconds
+const helloAsync = new Promise<string>((resolve) => {
+    setTimeout(() => {
+        resolve('Hello Async');
+    }, 2000);
+});
+helloAsync.then((value) => {
+    console.log(value); 
+});
