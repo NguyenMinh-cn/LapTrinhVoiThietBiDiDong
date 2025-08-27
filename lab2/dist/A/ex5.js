e7c0d8f7c9181163e9bb5a1a1ad53f04d724cc12
NguyenNgocHongMinh <nguyengochongminh@gmail.com>
2025-08-27 07:46:42 +0700

    hoan thanh bai 5 trong lab02

---
+++ lab2/dist/A/ex5.js
@@ -0,0 +1,12 @@
+"use strict";
+// 5. Create a function simulateTask(time) that returns a Promise resolving with "Task done" after time ms.
+function simulateTask(time) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Task done");
+        }, time);
+    });
+}
+simulateTask(5000).then((result) => {
+    console.log(result);
+});
---
+++ lab2/src/A/ex5.ts
@@ -0,0 +1,11 @@
+// 5. Create a function simulateTask(time) that returns a Promise resolving with "Task done" after time ms.
+function simulateTask(time: number) {
+    return new Promise((resolve) =>{
+        setTimeout(() => {
+            resolve("Task done");
+        }, time);
+    });
+}
+simulateTask(5000).then((result)=>{
+    console.log(result);
+})
