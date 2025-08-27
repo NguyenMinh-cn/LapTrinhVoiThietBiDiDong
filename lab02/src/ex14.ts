d131efe86dd4db072f0a7aae2862d3381c2ea4f8
NguyenNgocHongMinh <nguyengochongminh@gmail.com>
2025-08-27 07:15:07 +0700

    tao thu muc lam bai tạp tuan 2

--- lab02/src/ex14.ts
+++
@@ -1,9 +0,0 @@
-// 14. Write an async function that takes a number, waits 1 second, and returns the number × 3.
-async function ex14(num: number): Promise<number> {
-  await new Promise(resolve => setTimeout(resolve, 1000));
-  return num * 3;
-}
-
-ex14(15).then(result => {
-  console.log("Kết quả:", result);
-});
