fd2e9a41f235860400dbcdb236702a84737177b3
Hong Minh <nguyengochongminh@gmail.com>
2025-09-21 18:38:41 +0700

    Hoan thanh bai 3 tuan 5

---
+++ lab5/bai3/App.js
@@ -0,0 +1,61 @@
+import{ useEffect, useState } from "react";
+import { View, Text, FlatList, StyleSheet} from "react-native";
+
+type User = {
+  id: string;
+  name: string;
+  email: string;
+};
+
+export default function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://6830912f6205ab0d6c399b60.mockapi.io/user")
+      .then((res) => res.json())
+      .then((data: User[]) => {
+        setUsers(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error("Lỗi fetch:", err);
+      });
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={users}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.item}>
+            <Text style={styles.name}>{item.name}</Text>
+            <Text style={styles.email}>{item.email}</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "#fff",
+  },
+
+  item: {
+    padding: 12,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+  },
+  name: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  email: {
+    fontSize: 14,
+    color: "#555",
+  },
+});
